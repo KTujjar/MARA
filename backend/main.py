48498e3bbbd32ea7517aa78a530738eac231524c
KTujjar <khaled@tujjar.com>
2026-09-02 11:41:08 -0400

    added frontend with basic vite scaffold

--- backend/main.py
+++ backend/main.py
@@ -15,14 +15,3 @@ app.add_middleware(
 
 app.include_router(router)
 
-"""
-from graph.build_graph import build_graph
-
-
-if __name__ == "__main__":
-    query = input("What would you like to research?\n")
-
-    app = build_graph()
-    result = app.invoke({"query":query})
-    print(result["report"])
-"""
